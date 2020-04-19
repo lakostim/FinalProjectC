0dc372af9647a46dd53ee1d1542f4fd7c20b6df5
zeinz <zeinz@DESKTOP-PQCG1TL>
2020-04-19 18:18:26 -0400

    started entities

---
+++ HRAppLib/HRAppLib/Entities/Department.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAppLib.Entities
+{
+    public class Department
+    {
+        private readonly int _departmentNumber;
+        public int DepartmentNumber =>_departmentNumber;
+
+        private string _departmentName;
+        public string DepartmentName
+        {
+            get => _departmentName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Department name cannot be blank");
+                _departmentName = value;
+            }
+        }
+
+        private string _location;
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Location cannot be blank");
+                _location = value;
+            }
+        }
+
+        public Department(int departmentNumber, string departmentName, string location)
+        {
+            _departmentNumber = departmentNumber;
+            DepartmentName = departmentName;
+            Location = location;
+        }
+
+        public override string ToString() => $"{DepartmentNumber},{DepartmentName},{Location}";
+    }
+}
---
+++ HRAppLib/HRAppLib/Entities/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//entity to form employee data object
+namespace HRAppLib.Entities
+{
+    public class Employee
+    {
+        private readonly int _employeeId;
+        public int EmployeeId =>_employeeId;
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Employee name cannot be blank");
+                _name = value;
+            }
+        }
+
+        private float _hourlySalary;
+        public float HourlySalary
+        {
+            get => _hourlySalary;
+            set
+            {
+                if (value<15)
+                    throw new ArgumentException("Salary cannot be lesser than the minimum pay rate");
+                _hourlySalary = value;
+            }
+        }
+
+        public Department Department { get; set; }
+        public Employee(int employeeId, string name, float hourlySalary, Department department)
+        {
+            _employeeId = employeeId;
+            Name = name;
+            HourlySalary = hourlySalary;
+            Department=department;
+        }
+
+        public override string ToString() => $"{EmployeeId},{Name},{HourlySalary},{Department}";
+
+    }
+}
