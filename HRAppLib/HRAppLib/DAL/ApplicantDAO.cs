a54fe5cd151c8b117bc6da45265d663c2a2f1552
Ian Ibrahim <zax.zl.zl.zl@gmail.com>
2020-04-21 20:55:39 -0400

    #5 DAO

--- HRAppLib/HRAppLib/DAL/ApplicantDAO.cs
+++ HRAppLib/HRAppLib/DAL/ApplicantDAO.cs
@@ -11,7 +11,11 @@ namespace HRAppLib.DAL
     {
         private LibDS _libDS = new LibDS();
         private ApplicantsTableAdapter _appTA = new ApplicantsTableAdapter();
-        private DepartmentsTableAdapter departmentsTableAdapter = new DepartmentsTableAdapter():
+        private DepartmentsTableAdapter departmentsTableAdapter = new DepartmentsTableAdapter();
 
-    }
+         public void AddEmployeeRecord(string firstName, string lastName, string role, bool isEmployed, float hourlySalary, int department, int resumeID, string email, string phone)
+        {
+            _appTA.Insert(firstName,  lastName,  role, hourlySalary, resumeID, isEmployed, email, phone, department);
+        }
+            }
 }
